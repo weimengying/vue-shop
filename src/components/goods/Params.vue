c07dfca28d8b25c4a01256c3d9c47cd8c7b76d71
weimengying <weimengying515@163.com>
2020-06-30 14:48:11 +0800

    商品分类参数删除功能

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -308,6 +308,32 @@ export default {
         this.getParamsData()
         this.editDialogVisible = false
       })
+    },
+    //删除参数
+    async removeParams(attr_id) {
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该参数, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
+      //用户取消了删除的操作
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+      //删除的业务逻辑
+      const { data: res } = await this.$http.delete(
+        `categories/${this.cateId}/attributes/${attr_id}`
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除参数失败！')
+      }
+
+      this.$message.success('删除参数成功！')
+      this.getParamsData()
     }
   }
 }
